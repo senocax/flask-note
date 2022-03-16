a16ba5fbd001681fcf7dad98c7fed575e5c50de6
senocax <you@example.com>
2022-03-16 13:48:26 -0300

    finish project

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+from crypt import methods
 from re import U
-from flask import Flask, redirect, render_template, request, url_for
-from flask_sqlalchemy import SQLAlchemy
+from wsgiref.validate import validator
+from xml.dom.minidom import Identified
+from flask import Flask, redirect, render_template, request, request_started, url_for
 from flask_migrate import Migrate
+from forms import PersonaForm
+
+from models import *
+from database import *
 
 
 app= Flask(__name__,template_folder='./templates',static_folder='./templates/static')
@@ -17,26 +23,14 @@ FULL_URL_DB= f'postgresql://{USER_DB}:{PASS_DB}@{URL_DB}/{NAME_DB}'
 app.config['SQLALCHEMY_DATABASE_URI'] = FULL_URL_DB
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 
-#initialized sql alchemy objects
-db=SQLAlchemy(app)
+db.init_app(app)
 
 #configure flask-migrate
 migrate= Migrate()
 migrate.init_app(app, db)
 
-class Persona(db.Model):
-    id= db.Column(db.Integer, primary_key=True)
-    nombre=db.Column(db.String(250))
-    apellido=db.Column(db.String(250))
-    email=db.Column(db.String(250))
-
-    def __str__(self):
-        return (
-            f'id: {self.id}, '
-            f'nombre: {self.nombre}, '
-            f'apellido: {self.nombre}, '
-            f'email: {self.nombre}, '
-                )
+#configure flask-wtf
+app.config['SECRET_KEY'] = 'llave_secreta'
 
 
 @app.before_request
@@ -45,13 +39,14 @@ def before_request():
 
 @app.after_request
 def after_request(response):
+
     print('Despues de la peticion')
     return response
 
 @app.route('/')
 def index ():
 
-    personas = Persona.query.all()
+    personas = Persona.query.order_by('id')
     total_personas= Persona.query.count()
 
     cursos =['Python', 'Java', 'C++', 'Html', 'Css']
@@ -65,14 +60,46 @@ def index ():
     app.logger.debug(f'Cantidad de personas {total_personas}')
     return render_template('index.html', data= data, personas= personas, total_personas= total_personas )
 
-@app.route('/contacto/<nombre>/<int:edad>')
-def contacto(nombre,edad):
-    data ={
-        'titulo':'contacto',
-        'nombre':nombre,
-        'edad':edad
-    }
-    return render_template('contacto.html', data= data)
+@app.route('/contacto/<int:id>')
+def contacto(id):
+    persona = Persona.query.get(id)
+    app.logger.debug(f"la persona es {persona.id}")
+    return render_template('contacto.html', persona= persona)
+
+@app.route('/agregar', methods=['GET','POST'])
+def agregar():
+    persona = Persona()
+    personaForm = PersonaForm(obj = persona)
+    if (request.method == "POST") :
+        if personaForm.validate_on_submit():
+            personaForm.populate_obj(persona)
+            app.logger.debug(f"Se insertara en la Db {persona}")
+            db.session.add(persona)
+            db.session.commit()
+            return redirect(url_for("index"))
+
+    return render_template('agregar.html', forma = personaForm )
+@app.route('/editar/<int:id>', methods=['GET' , 'POST'])
+def editar(id):
+    persona = Persona.query.get(id)
+    personaForma= PersonaForm(obj=persona)
+    if(request.method == 'POST'):
+        if personaForma.validate_on_submit():
+            personaForma.populate_obj(persona)
+            app.logger.debug(f"Se edatara en la Db {persona}")
+            db.session.commit()
+            return redirect(url_for("index"))
+
+    return render_template('editar.html',persona = persona, forma = personaForma)
+
+@app.route('/eliminar/<int:id>')
+def eliminar(id):
+    persona = Persona.query.get(id)
+    app.logger.debug(f"Se eliminara en la Db {persona}")
+    db.session.delete(persona)
+    db.session.commit()
+    return redirect(url_for("index"))
+
 
 def query_string(): 
     print(request)
